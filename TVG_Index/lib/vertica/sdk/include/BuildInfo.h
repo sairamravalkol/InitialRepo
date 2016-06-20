40cd994398b4f8864a4faf29288014b078df4bbd
eyal.hayun@hpe.com <Password.500>
2016-06-20 11:41:19 +0300

    git init fix build

---
+++ TVG_Index/lib/vertica/sdk/include/BuildInfo.h
@@ -0,0 +1,18 @@
+/* 
+ * Copyright (c) 2014 Vertica Systems, an HP Company
+ *
+ * Description: Support code for UDx subsystem
+ *
+ * Create Date: Mon Oct  5 22:36:57 2015
+ */
+/* Link-time identification for VerticaSDK */
+#define VERTICA_BUILD_ID_Brand_Name       "Vertica Analytic Database"
+#define VERTICA_BUILD_ID_Brand_Version    "v7.2.0-0"
+#define VERTICA_BUILD_ID_SDK_Version      "7.2.0"
+#define VERTICA_BUILD_ID_Codename         "Excavator"
+#define VERTICA_BUILD_ID_Date             "Mon Oct  5 22:36:57 2015"
+#define VERTICA_BUILD_ID_Machine          "build-centos6"
+#define VERTICA_BUILD_ID_Branch           "releases/VER_7_2_RELEASE_BUILD_0_0_20151005"
+#define VERTICA_BUILD_ID_Revision         "174832"
+#define VERTICA_BUILD_ID_Checksum         "bd691bffb5af09de8c2f6c81f5db163b"
+/* end of this file */
---
+++ TVG_Index/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Vertica.h"
+using namespace std;
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
