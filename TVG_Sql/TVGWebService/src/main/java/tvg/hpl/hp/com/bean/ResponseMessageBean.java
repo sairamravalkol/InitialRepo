c62079115e68ffa6e6c67bf0172a946e967d4564
jiban-jyoti-sarma <jiban.jyoti.sarma@hpe.com>
2016-07-21 12:37:51 +0530

    package name redefined

---
+++ TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/bean/ResponseMessageBean.java
@@ -0,0 +1,27 @@
+package tvg.hpl.hp.com.bean;
+
+/**
+ * @author sarmaji
+ *
+ */
+
+public class ResponseMessageBean {
+	private String task_id;
+	private String status;
+	
+	public String getTask_id() {
+		return task_id;
+	}
+	public void setTask_id(String task_id) {
+		this.task_id = task_id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+
+}
