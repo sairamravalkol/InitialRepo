a2ad617050aa35dcc968169028a58e1313306c34
rsair <rsair@RSAIR1.asiapacific.hpqcorp.net>
2016-07-25 10:47:37 +0530

    partial code completion

--- TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/dao/ShowGraphDao.java
+++ TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/dao/ShowGraphDao.java
@@ -4,34 +4,66 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import tvg.hpl.hp.com.bean.StartBfsBean;
 import tvg.hpl.hp.com.util.DataBaseUtility;
 
+/**
+ * "ShowGraphDao" class represents to use vertica database to check StatusID  get corresponding
+ *  taskID from vertica and store it into StartBfsBean Instance.
+ * @author SAIRAM RAVALKOL
+ * @version 1.0 25/07/2016
+ *
+ */
+
 public class ShowGraphDao {
+	static Logger log = LoggerFactory.getLogger(ShowGraphDao.class);
+
 	private BasicDataSource datasource;
 	private Connection connection;
 	private PreparedStatement pstmt;
-	private Statement stmt;
 	private ResultSet resultset;
 
 	public ShowGraphDao() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void createTaskId(StartBfsBean queryBean) {
-		// TODO Auto-generated method stub
+	/**
+	 * Returns the StartBfsBean object having status and TaskId.
+	 * 
+	 * @param queryBean StartBfsBean object having taskId
+	 * @return instance of StartBfsBean.
+	 */
+	public StartBfsBean checkTaskIdStatus(StartBfsBean queryBean) {
 
-		if(queryBean != null){
+		// checking queryBean object is null or not
+		if(queryBean != null) {
 			datasource = DataBaseUtility.getVerticaDataSource();
 			try {
-				String query = "Insert in";
+				String taskId = queryBean.getTaskId();
+				String query ="SELECT task_id,request_status FROM tvg4tm.ws_user_query WHERE task_id = " + taskId;
+				System.out.println("Query : " + query);
+				log.info(query);
+
 				connection = datasource.getConnection();
+				connection.setAutoCommit(false);
 				pstmt = connection.prepareStatement(query);
 				resultset = pstmt.executeQuery();
+
+				// setting  task id and status code into queryBean instance
+				while(resultset.next())
+				{
+					queryBean.setTaskId(resultset.getString("task_id"));
+					queryBean.setStatus(resultset.getString("request_status"));
+				} // end while
+
+
+
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -42,12 +74,13 @@ public class ShowGraphDao {
 					if(connection != null)
 						connection.close();					
 				}catch(Exception ex){
-					
-				}
-			}
-			
-		}
-		
-	}
 
-}
+				} // inner catch
+			} // end finally block
+
+		} // end if
+
+		return queryBean;
+	} // end checkTaskIdStatus()
+
+} // end ShowGraphDao
--- TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/ws/ShowGraphService.java
+++ TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/ws/ShowGraphService.java
@@ -1,37 +1,167 @@
 package tvg.hpl.hp.com.ws;
 
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import tvg.hpl.hp.com.bean.ResponseErrorMessageBean;
+import tvg.hpl.hp.com.bean.ResponseMessageBean;
 import tvg.hpl.hp.com.bean.StartBfsBean;
-import tvg.hpl.hp.com.dao.GetSubGraph;
+import tvg.hpl.hp.com.dao.ShowGraphDao;
 import tvg.hpl.hp.com.dao.StartBfsDao;
-import tvg.hpl.hp.com.util.ManageAppProperties;
+import tvg.hpl.hp.com.util.GraphsonUtil;
 import tvg.hpl.hp.com.websocket.TVGWidget;
 
+/**
+ * 
+ * @author Sairam Ravalkol
+ * @version 1.0 22/07/2016
+ */
 @Path("/ShowGraph")
-public class ShowGraphService {
+public class ShowGraphService implements Runnable	{
 	static Logger log = LoggerFactory.getLogger(ShowGraphService.class);
+	private StartBfsBean queryBean;
+	private ShowGraphDao showGraphDao;
+	private StartBfsDao startbfsdao;
+	private String responseJsonMessage;
+	private String responseJsonErrorMessage;
+	private String jsonStringGraph;
 	
 	public ShowGraphService() {
 		// TODO Auto-generated constructor stub	
 	}
 	
+	public ShowGraphService(StartBfsBean querybeanpara) {
+		
+		this.queryBean = querybeanpara;
+	}
+	
+	/**
+	 * Method "showGraph" will send the response to client. As Success is 0 
+	 * @param taskId
+	 * @return 
+	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String  startBfs(@QueryParam("taskId") String taskId){			
-		if(taskId !=null){
-			Properties p = ManageAppProperties.getInstance().getApp_prop();
-			System.out.println("Show Graph");
-		}		
-		return "{tid:1, status :received}";
+	public Response showGraph(@QueryParam("tid") String taskId) {
+	
+		
+		if(taskId!=null && !taskId.equals("null") && !taskId.isEmpty())
+		{
+			queryBean = new StartBfsBean();
+			queryBean.setTaskId(taskId);
+			/**
+			 *  Checking status is completed or not
+			 */
+			showGraphDao = new ShowGraphDao();
+			StartBfsBean checkTaskIdStatus = showGraphDao.checkTaskIdStatus(queryBean);
+			if(checkTaskIdStatus.getStatus().equals("completed")) 
+			{
+			
+				ResponseMessageBean response = new ResponseMessageBean();
+				response.setTask_id(checkTaskIdStatus.getTaskId());
+				response.setStatus("0");
+				
+				ShowGraphService runnable = new ShowGraphService(checkTaskIdStatus);
+				System.out.println(checkTaskIdStatus.getPush_when_done());
+				//System.out.println(queryBean.getTaskId()+ " "+queryBean.getStatus());
+				Thread thread = new Thread(runnable);
+				thread.run();
+				
+
+				GraphsonUtil graphsonUtilObj = new GraphsonUtil();
+				
+			    responseJsonMessage = graphsonUtilObj.jsonResponseMessage(response);
+			   
+			   /**
+				 * Accept the request and send the response code 202(Accepted) along
+				 * with the taskId and status
+				 * 
+				 */
+				
+				return Response.status(202).entity(responseJsonMessage).build();
+			}
+			else
+			{
+				
+				ResponseErrorMessageBean errorMessageBean = new ResponseErrorMessageBean();
+				errorMessageBean.setErrorMessage("Task is not completed...");
+				GraphsonUtil graphsonUtil = new GraphsonUtil();
+				responseJsonErrorMessage = graphsonUtil.jsonResponseErrorMessage(errorMessageBean);
+
+				/**
+				 * Return the status code 400(Bad Request) along with the status
+				 * message
+				 */
+				return Response.status(400).entity(responseJsonErrorMessage).type(MediaType.APPLICATION_JSON).build();
+			}
+		}
+		else
+		{
+			ResponseErrorMessageBean errorMessageBean = new ResponseErrorMessageBean();
+			errorMessageBean.setErrorMessage("invalid parameter");
+			GraphsonUtil graphsonUtil = new GraphsonUtil();
+			responseJsonErrorMessage = graphsonUtil.jsonResponseErrorMessage(errorMessageBean);
+
+			/**
+			 * Return the status code 400(Bad Request) along with the status
+			 * message
+			 */
+			return Response.status(400).entity(responseJsonErrorMessage).type(MediaType.APPLICATION_JSON).build();
+			
+		}
+	}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+						
+			Map<String, List<List<String>>> subGraph = new HashMap<>();
+			StartBfsDao bfsDao = new StartBfsDao();
+			subGraph = bfsDao.getSubgraphWithTaskId(queryBean);
+			//System.out.println("SubGraph : "+subGraph);
+			System.out.println("taskId ::" + queryBean.getTaskId());
+
+			System.out.println("print:" + subGraph);
+
+			/**
+			 * Update the user request status
+			 * 
+			 */
+			startbfsdao = new StartBfsDao();
+			queryBean = startbfsdao.updateUserRequestStatus(queryBean);
+
+			/**
+			 * Pass the sub Graph to GraphsonUtil and convert it to the JSON format.
+			 * 
+			 */
+			
+			if (subGraph.size() > 0) {
+				GraphsonUtil graphsonObj = new GraphsonUtil();
+				graphsonObj.subGraphJson(subGraph);
+				
+				
+				/**
+				 * Parse json object to Json String
+				 * 
+				 */
+				jsonStringGraph=graphsonObj.parseGraphsonFormat();
+				/**
+				 * write results to the TVGwidget through WebSocket
+				 */
+				TVGWidget widgetObj = new TVGWidget();
+				widgetObj.sendSubGraphOverSocket(jsonStringGraph);
+				
+			}
+
 	}
 }
