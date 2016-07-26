545d90848bc10a4f1f93dd2059632eeeb9b3eff8
R <sairam.r@hpe.com>
2016-07-26 11:57:58 +0530

    TVG webservice api

--- TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/ws/GetGraphService.java
+++ TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/ws/GetGraphService.java
@@ -1,25 +1,40 @@
 package tvg.hpl.hp.com.ws;
 
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import tvg.hpl.hp.com.bean.ResponseErrorMessageBean;
+import tvg.hpl.hp.com.bean.ResponseMessageBean;
 import tvg.hpl.hp.com.bean.StartBfsBean;
-import tvg.hpl.hp.com.dao.GetSubGraph;
+import tvg.hpl.hp.com.dao.ShowGraphDao;
 import tvg.hpl.hp.com.dao.StartBfsDao;
-import tvg.hpl.hp.com.util.ManageAppProperties;
-import tvg.hpl.hp.com.websocket.TVGWidget;
+import tvg.hpl.hp.com.util.GraphsonUtil;
 
 @Path("/GetGraph")
 public class GetGraphService {
 	static Logger log = LoggerFactory.getLogger(GetGraphService.class);
+	private StartBfsBean queryBean;
+	private ShowGraphDao showGraphDao;
+	private StartBfsDao startbfsdao;
+	private String responseJsonMessage;
+	private String responseJsonErrorMessage;
+	private String jsonStringGraph;
 	
 	public GetGraphService() {
 		// TODO Auto-generated constructor stub
@@ -28,20 +43,90 @@ public class GetGraphService {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String  startBfs(@QueryParam("taskId") String taskId){
+	public Response getGraph(@QueryParam("tid")String taskId) throws JSONException {
 		
-		if(taskId !=null){						
-			Properties p = ManageAppProperties.getInstance().getApp_prop();
-			System.out.println("Get Graph Service");
-			
+		if(taskId.matches("^[0-9]*$") && taskId!=null && !taskId.equals("null") && !taskId.isEmpty()) {
+			queryBean = new StartBfsBean();
+			queryBean.setTaskId(taskId);
+
+			showGraphDao = new ShowGraphDao();
+			 queryBean = showGraphDao.checkTaskIdStatus(queryBean);
+			 
+			 if(queryBean.getStatus().equals("completed")) {
+				JsonObject jsonObjectBuilder = Json.createObjectBuilder().build();
+				jsonObjectBuilder.put("taskId", queryBean.getTaskId());
+				jsonObjectBuilder.put("Status", "0");
+				
+				Map<String, List<List<String>>> subGraph = new HashMap<>();
+				StartBfsDao bfsDao = new StartBfsDao();
+				subGraph = bfsDao.getSubgraphWithTaskId(queryBean);
+				//System.out.println("SubGraph : "+subGraph);
+				System.out.println("taskId ::" + queryBean.getTaskId());
+
+				System.out.println("print:" + subGraph);
+				if (subGraph.size() > 0) {
+					
+					GraphsonUtil graphsonObj = new GraphsonUtil();
+					graphsonObj.subGraphJson(subGraph);
+					
+					
+					/**
+					 * Parse json object to Json String
+					 * 
+					 */
+					jsonStringGraph=graphsonObj.parseGraphsonFormat();
+					jsonObjectBuilder.add("result", jsonStringGraph);
+					System.out.println(jsonObjectBuilder);
+					//response.setResult(jsonStringGraph);
+					
+				   
+				 
+				   
+				   return Response.status(202).entity(null).type(MediaType.APPLICATION_JSON).build();
+				
+				}
+				else
+				{
+					ResponseErrorMessageBean errorMessageBean = new ResponseErrorMessageBean();
+					errorMessageBean.setErrorMessage(" Graph is not available");
+					GraphsonUtil graphson = new GraphsonUtil();
+					responseJsonErrorMessage = graphson.jsonResponseErrorMessage(errorMessageBean);
+					return Response.status(400).entity(responseJsonErrorMessage).type(MediaType.APPLICATION_JSON).build();
+				}
+					 
+				
+			 }
+			 else
+			 {
+				 ResponseErrorMessageBean errorMessageBean = new ResponseErrorMessageBean();
+					errorMessageBean.setErrorMessage("Status : completed");
+					GraphsonUtil graphsonUtil = new GraphsonUtil();
+					responseJsonErrorMessage = graphsonUtil.jsonResponseErrorMessage(errorMessageBean);
+
+					/**
+					 * Return the status code 400(Bad Request) along with the status
+					 * message
+					 */
+					return Response.status(400).entity(responseJsonErrorMessage).type(MediaType.APPLICATION_JSON).build();
+					
+ 
+			 }
+		} 
+		else
+		{
+			ResponseErrorMessageBean errorMessageBean = new ResponseErrorMessageBean();
+			errorMessageBean.setErrorMessage("invalid parameter");
+			GraphsonUtil graphsonUtil = new GraphsonUtil();
+			responseJsonErrorMessage = graphsonUtil.jsonResponseErrorMessage(errorMessageBean);
+
 			/**
-			 * First check the status of the Taskid In the user_query table if its completed 
-			 * query the result table and return the results else return the taskid and the status
+			 * Return the status code 400(Bad Request) along with the status
+			 * message
 			 */
+			return Response.status(400).entity(responseJsonErrorMessage).type(MediaType.APPLICATION_JSON).build();
 			
-		}		
-		return "{tid:1, status :received}";
+		}
+		
 	}
-	
 
 }
