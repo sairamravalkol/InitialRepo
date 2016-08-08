5580a03936583e54970fa9d38d78dc3b06303fce
jiban <mailto.jiban@gmail.com>
2016-08-08 19:31:09 +0530

    Grpah metadata information

--- TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/services/GetNodeMetaData.java
+++ TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/services/GetNodeMetaData.java
@@ -1,30 +1,31 @@
-package tvg.hpl.hp.com.dao;
+package tvg.hpl.hp.com.services;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import tvg.hpl.hp.com.bean.QueryResultBean;
 import tvg.hpl.hp.com.bean.StartBfsBean;
+import tvg.hpl.hp.com.dao.StartBfsDao;
 
 public class GetNodeMetaData implements Runnable {
 	static Map<String, List<String>> nodeMetaDataMap = new HashMap<>();
-	private StartBfsBean queryBean;
-	public GetNodeMetaData(StartBfsBean bfsBean){
-		queryBean = bfsBean;		
+	private String taskId;
+	public GetNodeMetaData(String taskIdPara){
+		taskId = taskIdPara;
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		StartBfsDao startbfsDao = new StartBfsDao();
-		Set<String> vertices = startbfsDao.getAllVerticesByTaskId(queryBean);
+		Set<String> vertices = startbfsDao.getAllVerticesByTaskId(taskId);
 		StringBuffer bf = new StringBuffer();		
 		for(String vertex :vertices){
 			bf.append(vertex).append(",");			
 		}
 		bf.setLength(bf.length()-1);
-		System.out.println("Executor service:"+ bf.toString());
 		nodeMetaDataMap =	startbfsDao.getNodeMetaDataList(bf.toString());
 	}
 	
