dbdea6ac6bc7352b42e50210749510dcc4edbb8d
rsair <rsair@RSAIR1.asiapacific.hpqcorp.net>
2016-08-08 12:04:33 +0530

    GraphStatistics Api Updated

--- TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/dao/GetGraphStatisticsDao.java
+++ TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/dao/GetGraphStatisticsDao.java
@@ -8,41 +8,84 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import tvg.hpl.hp.com.bean.StartBfsBean;
+import tvg.hpl.hp.com.bean.StatisticsBean;
 import tvg.hpl.hp.com.util.DataBaseUtility;
 
 public class GetGraphStatisticsDao {
 	private BasicDataSource datasource;
 	private Connection connection;
-	private PreparedStatement pstmt;
-	private Statement stmt;
-	private ResultSet resultset;
+	private PreparedStatement pstmt1;
+	private PreparedStatement pstmt2;
+	private ResultSet resultset1;
+	private ResultSet resultset2;
+	private Set<String> vertices;
+	private StatisticsBean statBean;
+	private int count=0;
+	private List<Object> list;
 
 	public GetGraphStatisticsDao() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void createTaskId(StartBfsBean queryBean) {
+	public List<Object> fetch(StartBfsBean queryBean) {
 		// TODO Auto-generated method stub
 
 		if(queryBean != null){
 			datasource = DataBaseUtility.getVerticaDataSource();
 			try {
-				String query = "Insert in";
+				 vertices = new HashSet<>();
+				 list = new ArrayList<>();
+				String userquery = "SELECT task_id,start_time,end_time,hop FROM tvg4tm.ws_user_query "
+							+ "WHERE task_id="+ queryBean.getTaskId();
+				String wsquery = "SELECT source,destination FROM tvg4tm.ws_result "
+						+ "WHERE task_id="+ queryBean.getTaskId();
+				System.out.println("Query : " + userquery);
+				System.out.println("Query : " + wsquery);
+				
 				connection = datasource.getConnection();
-				pstmt = connection.prepareStatement(query);
-				resultset = pstmt.executeQuery();
+				pstmt1 = connection.prepareStatement(userquery);
+				pstmt2 = connection.prepareStatement(wsquery);
+				resultset1 = pstmt1.executeQuery();
+				resultset2 = pstmt2.executeQuery();
+				
+				while(resultset1.next()) {
+					queryBean.setTaskId(resultset1.getString("task_id"));
+					queryBean.setStartTime(resultset1.getString("start_time"));
+					queryBean.setEndTime(resultset1.getString("end_time"));
+					queryBean.setHop(resultset1.getString("hop"));
+				}
+				list.add(queryBean);
+				
+				statBean = new StatisticsBean();
+				while(resultset2.next()) {
+			        count++;
+					vertices.add(resultset2.getString("source"));
+					vertices.add(resultset2.getString("destination"));
+				}
+				vertices.size();
+				statBean.setNum_edges(count);
+				statBean.setNum_vertices(vertices.size());
+				
+				list.add(statBean);
+				
+				
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}finally{
 				try{
-					if(pstmt != null)
-						pstmt.close();
+					if(pstmt1 != null)
+						pstmt1.close();
+					if(pstmt2 != null)
+						pstmt2.close();
 					if(connection != null)
 						connection.close();					
 				}catch(Exception ex){
@@ -51,6 +94,7 @@ public class GetGraphStatisticsDao {
 			}
 			
 		}
+		return list;
 		
 	}
 
--- TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/ws/GetGraphStatisticsService.java
+++ TVG_Sql/TVGWebService/src/main/java/tvg/hpl/hp/com/ws/GetGraphStatisticsService.java
@@ -1,40 +1,113 @@
 package tvg.hpl.hp.com.ws;
 
-import java.util.Properties;
-
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import tvg.hpl.hp.com.bean.ResponseErrorMessageBean;
 import tvg.hpl.hp.com.bean.StartBfsBean;
-import tvg.hpl.hp.com.dao.GetSubGraph;
-import tvg.hpl.hp.com.dao.StartBfsDao;
-import tvg.hpl.hp.com.util.ManageAppProperties;
-import tvg.hpl.hp.com.websocket.TVGWidget;
+import tvg.hpl.hp.com.bean.StatisticsBean;
+import tvg.hpl.hp.com.dao.GetGraphStatisticsDao;
+import tvg.hpl.hp.com.dao.ShowGraphDao;
+import tvg.hpl.hp.com.util.GraphsonUtil;
 
+/**
+ * The API will return different graph statistics that are TBD. In particular 
+ * it will return the time range that the graph correspond to, the seed vertices 
+ * and hop count. These should be stored as part of the handling of StartBFS. 
+ * Additional statistics on a result graph (such as performance related statistics)
+ *  might be required and will returned in a formatted JSON.
+ * @author JIBAN SHARAMA JYOTI
+ * @author SAIRAM RAVALKOL
+ * @version 1.0.0 07/08/2016
+ *
+ */
 @Path("/GetGraphStatistics")
 public class GetGraphStatisticsService {
 	static Logger log = LoggerFactory.getLogger(GetGraphStatisticsService.class);
-		
+	private StartBfsBean queryBean;
+	private ShowGraphDao showGraphDao;
+	private String responseJsonErrorMessage;
+	private StatisticsBean statBean;
+	private GetGraphStatisticsDao getGraphStatisticsDao;
+
 	public GetGraphStatisticsService() {
 		// TODO Auto-generated constructor stub
-		
+
 	}
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String  startBfs(@QueryParam("taskId") String taskId){
-		
-		if(taskId !=null){						
-			Properties p = ManageAppProperties.getInstance().getApp_prop();
-			System.out.println("Get Graph Statistics");
-		}		
-		return "{tid:1, status :received}";
+	public Response getGraphStatistics(@QueryParam("tid") String taskId) {
+		if (taskId.matches("^[0-9]*$") && taskId != null && !taskId.equals("null") && !taskId.isEmpty()) {
+			queryBean = new StartBfsBean();
+			queryBean.setTaskId(taskId);
+			/**
+			 * Checking status is completed or not
+			 */
+			showGraphDao = new ShowGraphDao();
+			StartBfsBean checkTaskIdStatus = showGraphDao.checkTaskIdStatus(queryBean);
+			String status = checkTaskIdStatus.getStatus();
+			// System.out.println(status!=null);
+			if (status != null && checkTaskIdStatus.getStatus().equals("completed")) {
+				getGraphStatisticsDao = new GetGraphStatisticsDao();
+				List<Object> list = getGraphStatisticsDao.fetch(checkTaskIdStatus);
+				queryBean = (StartBfsBean) list.get(0);
+				statBean = (StatisticsBean) list.get(1);
+
+				/** putting GraphStatistics  properties into JSON Object */
+				JSONObject json = new JSONObject();
+				try {
+					json.put("tid", queryBean.getTaskId());
+					json.put("num_vertices", statBean.getNum_vertices());
+					json.put("num_edges", statBean.getNum_edges());
+					json.put("start_time", queryBean.getStartTime());
+					json.put("end_time", queryBean.getEndTime());
+					json.put("depth", queryBean.getHop());
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				return Response.status(Status.ACCEPTED).entity(json.toString()).type(MediaType.APPLICATION_JSON)
+						.build();
+
+			} else {
+
+				ResponseErrorMessageBean errorMessageBean = new ResponseErrorMessageBean();
+				errorMessageBean.setErrorMessage("Status : Not Completed");
+				GraphsonUtil graphsonUtil = new GraphsonUtil();
+				responseJsonErrorMessage = graphsonUtil.jsonResponseErrorMessage(errorMessageBean);
+
+				/**
+				 * Return the status code 400(Bad Request) along with the status
+				 * message
+				 */
+				return Response.status(400).entity(responseJsonErrorMessage).type(MediaType.APPLICATION_JSON).build();
+			}
+		} else {
+			ResponseErrorMessageBean errorMessageBean = new ResponseErrorMessageBean();
+			errorMessageBean.setErrorMessage("invalid parameter");
+			GraphsonUtil graphsonUtil = new GraphsonUtil();
+			responseJsonErrorMessage = graphsonUtil.jsonResponseErrorMessage(errorMessageBean);
+
+			/**
+			 * Return the status code 400(Bad Request) along with the status
+			 * message
+			 */
+			return Response.status(400).entity(responseJsonErrorMessage).type(MediaType.APPLICATION_JSON).build();
+
+		}
 	}
-	
+
 }
